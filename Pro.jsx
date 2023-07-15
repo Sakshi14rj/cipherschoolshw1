7bd85395a1851b57ed487891872087659dcfca34
Sakshi <78091453+Sakshi14rj@users.noreply.github.com>
2023-07-15 20:55:04 +0530

    Create Pro.jsx

---
+++ Pro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Product(props) {
+  return (
+    <div>
+      <h3>{props.name}</h3>
+      <p>Price: {props.price}</p>
+    </div>
+  );
+}
+
+export default Product;
